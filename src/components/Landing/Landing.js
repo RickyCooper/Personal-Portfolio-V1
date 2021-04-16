f0bd2da287af9ec5194590c18a941dbdfdc1a0a1
Ricky Cooper <61281472+kr0mby@users.noreply.github.com>
2021-04-16 16:41:00 +0100

    Landing page background img
    background image will follow the direction of the mouse using an event listener.

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,27 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
 import SectionWrap from '../UI/SectionWrap/SectionWrap';
 import Social from '../UI/Social/Social';
 import styles from './landing.module.scss';
 
 const Landing = () => {
+    useEffect(() => {
+        const el = document.getElementById(`landing`);
+        el.addEventListener(`mousemove`, (e) => {
+            el.setAttribute(
+                `style`,
+                `background-position: ${(e.pageX * 100) / screen.availWidth}% ${
+                    (e.pageY * 100) / screen.availHeight
+                }%;`,
+            );
+        });
+    }, []);
+
+    // [ INFO ] SectionWrap takes classStyle and className, what this does is...
+    // allows me to style the children of this <SectionWrap> and no other
+
     return (
-        <SectionWrap>
-            <header>
-                <h1>RICKY</h1>
-                <h1>COOPER</h1>
-                <div className={styles.yellowUnderline}></div>
-                <ul>
-                    <li>WEB DEVELOPER</li>
-                    <li>UI / UX DESIGNER</li>
-                </ul>
-            </header>
-            <Social vertical />
-            <div className={styles.ViewMyWork}>
-                <a href="#work">
-                    <p>VIEW MY WORK</p>
-                    <span className={styles.arrowDown} />
-                </a>
-            </div>
+        <SectionWrap classStyle={styles} className="LandingWrap" id="landing">
+            <main id="wrapper">
+                <header>
+                    <h1>RICKY</h1>
+                    <h1>COOPER</h1>
+                    <div className={styles.yellowUnderline}></div>
+                    <ul>
+                        <li>WEB DEVELOPER</li>
+                        <li>UI / UX DESIGNER</li>
+                    </ul>
+                </header>
+                <div className={styles.ViewMyWork}>
+                    <a href="#work">
+                        <p>VIEW MY WORK</p>
+                        <span className={styles.arrowDown} />
+                    </a>
+                </div>
+                <Social vertical />
+            </main>
         </SectionWrap>
     );
 };
